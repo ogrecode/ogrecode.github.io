2f5622ae9e20f0bb823a0e09aaae0344f57eccb0
himanshu-adcat <himanshus45@hdfclife.com>
2019-06-11 20:44:31 +0530

    new page to test

---
+++ precache-manifest.4c6350e25e7f87dec873ed177a7e5bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca17f0ef6f0362576dee60293842e6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68cfe2ac1670a03c9001",
+    "url": "/static/css/main.da5b50cd.chunk.css"
+  },
+  {
+    "revision": "817deb8df975bfc4c738",
+    "url": "/static/js/2.19c7b8d6.chunk.js"
+  },
+  {
+    "revision": "68cfe2ac1670a03c9001",
+    "url": "/static/js/main.1dd9b89b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
